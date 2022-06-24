055a924c9baf8f694061c6ad7637b7452b1c2ab5
erlage <erlage.inc@gmail.com>
2022-06-24 12:55:00 +0530

    Remove unncessary information from results page

--- webdriver-ts-results/src/App.tsx
+++ webdriver-ts-results/src/App.tsx
@@ -10,25 +10,14 @@ const App = () => {
         <h2>Results for js web frameworks benchmark - official run</h2>
         <p>A description of the benchmark and the source code and can be found in the github <a href="https://github.com/krausest/js-framework-benchmark">repository</a>.</p>
       </div>) :
-      (<p>Warning: These results are preliminary - use with caution (they may e.g. be from different browser versions). Official results are published on the <a href="https://krausest.github.io/js-framework-benchmark/index.html">results page</a>.</p>);
+      (<p>Warning: These results are preliminary - use with caution.</p>);
 
     return (
       <div>
         {disclaimer}
-        <p>The benchmark was run on a Razer Blade 15 Advanced (i7-8750H, 64 GB RAM, Fedora 36 (Linux 5.17.3, mitigations=off, Wayland), Chrome  102.0.5005.61 (64-bit))
-            using the puppeteer benchmark driver.
-        </p>
          <SelectionBar/>
          <ResultTable type={FrameworkType.KEYED}/>
          <ResultTable type={FrameworkType.NON_KEYED}/>
-
-          <h3>Known issues and notes</h3>
-          {knownIssues.map(issue =>
-            <dl key={issue.issue.toFixed()} id={issue.issue.toFixed()}>
-              <dt><a target="_blank" rel="noopener noreferrer" href={issue.link}>{issue.issue.toFixed()}</a></dt>
-              <dd>{issue.text}</dd>
-            </dl>
-          )}
       </div>)
 }
 
